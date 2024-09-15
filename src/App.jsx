48b145d121a00c25c37fa0e371487260fab75357
Your Name <freelancermd82@gmail.com>
2024-09-15 08:31:59 +0600

    deleted client

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,29 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CardCoffee from './components/CardCoffee';
+import { useState } from 'react';
 
 function App() {
 
-  const coffees = useLoaderData();
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees)
 
   return (
-    <>
+    <div className='m-20'>
 
-      <h1 className='text-6xl text-purple-500'>Hot cold coffee: {coffees.length}</h1>
-      <div>
+      <h1 className='text-6xl text-center text-purple-500 my-8'>Hot cold coffee: {coffees.length}</h1>
+      <div className='grid md:grid-cols-2 gap-4'>
         {
           coffees.map(coffee => <CardCoffee
           key={coffee._id}
           coffee={coffee}
+          coffees={coffees}
+          setCoffees={setCoffees}
           ></CardCoffee>)
         }
       </div>
 
-    </>
+    </div>
   )
 }
 
--- src/components/CardCoffee.jsx
+++ src/components/CardCoffee.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
-const CardCoffee = ({ coffee }) => {
+const CardCoffee = ({ coffee, coffees, setCoffees }) => {
 
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your coffee has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = coffees.filter(cof => cof._id !== _id);
+                            setCoffees(remaining);
+                        }
+                    })
+            }
+        });
+
+    }
+
+
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <figure>
@@ -10,11 +48,23 @@ const CardCoffee = ({ coffee }) => {
                     src={photo}
                     alt="Movie" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>Click the button to watch on Jetflix app.</p>
+            <div className="flex justify-between w-full pr-3">
+                <div className='ps-3 mt-3'>
+                    <h2 className="card-title">{name}</h2>
+                    <p>{quantity}</p>
+                    <p>{supplier}</p>
+                    <p>{taste}</p>
+                    <p>{category}</p>
+                    <p>{details}</p>
+                </div>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
+                    <div className="join join-vertical space-y-4 mt-3">
+                        <button className="btn join-item">View</button>
+                        <button className="btn join-item">Edit</button>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn join-item">X</button>
+                    </div>
                 </div>
             </div>
         </div>
